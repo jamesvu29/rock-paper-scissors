cf17c015f8f69e8dad5e606e64efa628e8bdd72b
James Vu <itsjamie99@gmail.com>
2022-05-12 01:52:54 +1000

    Create working game with case insensitive prompt

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 function computerPlay() {
-    let arr = ["Rock", "Paper", "Scissor"];
+    let arr = ["rock", "paper", "scissor"];
     return arr[Math.floor(Math.random()*arr.length)]
 }
 
-computerPlay()
-
 function playround(playerSelection, computerSelection) {
+    
+
+    if(playerSelection.toLowerCase()===computerSelection) {
+        return "The game is a draw."
+    }
+
+    else if(playerSelection.toLowerCase()==="rock"){
+        if(computerSelection==="paper"){
+            return "You lose! paper beats rock."
+        }
+        else {
+            return "You win! rock beats scissor."
+        }
+    }
+    else if(playerSelection.toLowerCase()==="paper") {
+        if(computerSelection==="scissor") {
+            return "You lose! scissor beats paper."
+        }
+        else {
+            return "You win! paper beats rock."
+        }
+    }
+    else if(playerSelection.toLowerCase()==="scissor") {
+        if(computerSelection==="rock") {
+            return "You lose! rock beats scissor."
+        }
+        else {
+            return "You win! scissor beats paper."
+        }
+    }
 
 }
+
+const playerSelection = "Rock";
+const computerSelection= computerPlay()
+console.log(playround(playerSelection,computerSelection))

257fadd4fb18f4eee302286d1025c7288184cbcc
James Vu <itsjamie99@gmail.com>
2022-05-12 01:09:25 +1000

    created index and computerPlay func

---
+++ app.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let arr = ["Rock", "Paper", "Scissor"];
+    return arr[Math.floor(Math.random()*arr.length)]
+}
+
+computerPlay()
+
+function playround(playerSelection, computerSelection) {
+
+}

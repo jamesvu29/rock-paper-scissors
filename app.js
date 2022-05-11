d915c28f6be177237042c13d29b2d5a18324df0b
James Vu <itsjamie99@gmail.com>
2022-05-12 02:19:22 +1000

    Create best of 5 rounds on game

--- app.js
+++ app.js
@@ -37,6 +37,39 @@ function playround(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = "Rock";
-const computerSelection= computerPlay()
-console.log(playround(playerSelection,computerSelection))
+
+function game() {
+    let playerwin=0;
+    let computerwin=0;
+
+    for (let i=0; i<5; i++) {
+        let computerSelection= computerPlay();
+        let playerSelection = prompt("Choose your weapon");
+        console.log(playround(playerSelection,computerSelection));
+
+        if(playround(playerSelection,computerSelection).includes("win")==true) {
+            playerwin += 1;
+        }
+        else if (playround(playerSelection,computerSelection).includes("draw")==true) {
+            playerwin +=1;
+            computerwin +=1;
+        }
+        else {
+            computerwin += 1;
+        }
+
+        console.log(playerwin, computerwin);
+    }
+
+    if(computerwin>playerwin) {
+        return "You lose."
+    }
+    else if (computerwin===playerwin) {
+        return "A draw game."
+    }
+    else {
+        return "You win."
+    }
+}
+
+console.log(game())
